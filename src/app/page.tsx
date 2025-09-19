36f4d057f8259e55c6ebdd0a0dd5c3bdc5705611
vitaleymulikacatchup <vitaley.mulika.catchup@gmail.com>
2025-09-19 21:51:17 +0300

    Update project files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,7 +13,6 @@ export default function Home() {
       <div id="nav" data-section="nav">
         <NavbarStyleMinimal
           logoSrc="/images/logo.svg"
-          logoAlt="AuroraSaaS Logo"
           buttonText="Get Demo"
           onButtonClick={() => {}}
         />
